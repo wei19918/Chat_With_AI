0beb628e4601bbe9037f1e44ba41f69e02762385
Will <wei19918@hotmail.com>
2023-02-05 17:45:29 -0600

    add main chat script

--- chat.py
+++ chat.py
@@ -10,25 +10,30 @@ openai.api_key = os.getenv('OPEN_API_KEY')
 
 # Set up the model and prompt
 model_engine = "text-davinci-003"
-running_flag = True
 
-while running_flag:
-    try:
-        prompt = input("Type here:")
 
-        # Generate a response
-        completion = openai.Completion.create(
-            engine=model_engine,
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        response = completion.choices[0].text
-        print(f"OpenAI: {response}\n\n")
-    except Exception as e:
-        print(f"Exit with: {e}")
-        running_flag = False
+def run_chat():
+    running_flag = True
+    while running_flag:
+        try:
+            prompt = input("Type here:")
 
-print("All done")
+            # Generate a response
+            completion = openai.Completion.create(
+                engine=model_engine,
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+            response = completion.choices[0].text
+            print(f"OpenAI: {response}\n\n")
+        except Exception as e:
+            print(f"Exit with: {e}")
+            running_flag = False
+
+
+if __name__ == '__main__':
+    run_chat()
+    print("All done")
